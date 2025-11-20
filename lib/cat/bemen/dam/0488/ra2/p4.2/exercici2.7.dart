89f0f9f82773d3529a87db4b3a5154aae6d3a7b8
Roman <tu_email@example.com>
2025-11-20 20:59:39 +0100

    2.7 acabado (espero)

---
+++ lib/cat/bemen/dam/0488/ra2/p4.2/exercici2.7.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'providers/counter_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CounterProvider(),
+      child: const MyApp(),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Exercici 2.7 - Provider',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
